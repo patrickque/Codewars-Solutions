e49e14756b18a70d5a3405b4a2dafa9c7a09424a
Patrick Pisuc <pisucpatrick@mail.com>
2022-07-27 17:17:39 +0300

    Find the smallest integer in the array

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -53,12 +53,31 @@ const greet = () => "hello world!";
 
 
 //solution
-const invert = (arr) => arr.map(x => x * -1)
+const invert = (arr) => arr.map(x => x * -1);
 
 
 ////Implement a function which convert the given boolean value into its string representation.
 // Note: Only valid inputs will be given.
 
 //solution
-const booleanToString = b => b ? 'true' : 'false'
+const booleanToString = b => b ? 'true' : 'false';
 
+
+////Given an array of integers your solution should find the smallest integer.
+// For example:
+
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      
+    }
+  }
+
+//solution
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      return Math.min(...args);
+    }
+  }
