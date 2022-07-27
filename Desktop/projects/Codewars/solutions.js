38a6b7961e726a581f4d78bf17cabb232364c914
Patrick Pisuc <pisucpatrick@mail.com>
2022-07-27 16:39:07 +0300

    AreYouPlayingBanjo

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -1,4 +1,4 @@
-// This code does not execute properly. Try to figure out why.
+//// This code does not execute properly. Try to figure out why.
 
 function multiply(a, b){
     a * b
@@ -8,3 +8,29 @@ function multiply(a, b){
 
 const multiply = (a, b) => a * b
   
+
+//// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo" 
+// name + " does not play banjo"
+// Names given are always valid strings.
+
+function areYouPlayingBanjo(name) {
+    // Implement me
+    return name;
+  }
+
+//solution 
+
+function areYouPlayingBanjo(name) {
+    if (typeof name === 'string') {
+        if (name.charAt(0) === 'R' || name.charAt(0) === "r"){
+        return name + " plays banjo"
+      } else {
+        return name + " does not play banjo"
+      }
+    }
+  }
