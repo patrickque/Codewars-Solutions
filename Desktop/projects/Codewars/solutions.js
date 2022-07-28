aa27c9d8e51508887782ae0cd2866b4a3765ba43
Patrick Pisuc <pisucpatrick@mail.com>
2022-07-28 10:01:55 +0300

    Grasshopper - Personalized Message

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -135,3 +135,13 @@ function removeChar(str){
   let result = str.slice(1, -1)
   return result
 }
+
+//// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+// Use conditionals to return the proper message:
+// case               	   return
+// name equals owner	     'Hello boss'
+// otherwise	             'Hello guest'
+
+
+//solution
+const greet = (name, owner) => name === owner ? 'Hello boss' : 'Hello guest'
