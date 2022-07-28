fecb56590f130e6944ad5fa65e7fdb1432ab4a4f
Patrick Pisuc <pisucpatrick@mail.com>
2022-07-28 09:59:53 +0300

    Remove First and Last Character

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -124,3 +124,14 @@ function findNeedle(haystack) {
   let indexFound = haystack.indexOf('needle')
   return `found the needle at position ${indexFound}`
 }
+
+
+//// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string.
+//  You're given one parameter, the original string.  You don't have to worry with strings with less than two characters.
+
+
+//solution
+function removeChar(str){
+  let result = str.slice(1, -1)
+  return result
+}
