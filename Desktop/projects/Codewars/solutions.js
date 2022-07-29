c1ec99e62ad2cd18fc9f36790d98621d8a8da41b
Patrick Pisuc <pisucpatrick@mail.com>
2022-07-29 19:29:58 +0300

    Sum of positive

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -164,3 +164,18 @@ const max = arr => {
 const min = arr => {
   return Math.min(...arr)
 }
+
+//// You get an array of numbers, return the sum of all of the positives ones.
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+// Note: if there is nothing to sum, the sum is default to 0.
+
+//solution
+function positiveSum(arr) {
+  let sum = 0
+  for (let i = 0; i < arr.length; i++){
+    if(arr[i] > 0) {
+       sum += arr[i]
+    }
+  }
+  return sum
+}
