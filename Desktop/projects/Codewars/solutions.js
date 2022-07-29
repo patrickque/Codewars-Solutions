e19f002d766f082e6809859260d0224683453676
Patrick Pisuc <pisucpatrick@mail.com>
2022-07-29 19:32:26 +0300

    You only need one - Beginner

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -179,3 +179,12 @@ function positiveSum(arr) {
   }
   return sum
 }
+
+////You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+// Array can contain numbers or strings. X can be either.
+// Return true if the array contains the value, false if not.
+
+//solution
+function check(a, x) {
+  return a.includes(x)
+}
