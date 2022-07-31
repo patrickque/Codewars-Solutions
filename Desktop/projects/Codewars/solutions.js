e2c547cd93770deb993b8c55d491ac33f7614dd1
Patrick Pisuc <pisucpatrick@mail.com>
2022-07-31 12:07:42 +0300

    Square(n) Sum

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -386,3 +386,16 @@ function basicOp(operation, value1, value2){
     return value1 / value2
   }
 }
+
+////Complete the square sum function so that it squares each number passed into it and then sums the results together.
+// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+//solution
+function squareSum(numbers){
+  if (numbers.length === 0) {
+    return 0
+  } else {
+  let square = numbers.map((x, i) => x * x)
+  return square.reduce((acc, c) => acc + c)
+  }
+}
