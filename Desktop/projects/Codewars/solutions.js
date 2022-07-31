66a1f1a1c6780748bc73c528a18a5a59d3a4b979
Patrick Pisuc <pisucpatrick@mail.com>
2022-07-31 10:29:36 +0300

    Convert boolean values to strings 'yes' and 'no'

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -298,4 +298,13 @@ function noSpace(x){
 }
 
 
+////Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
+//solution
+function boolToWord( bool ){
+  if (bool){
+    return 'Yes'
+  } else {
+    return 'No'
+  }
+}
