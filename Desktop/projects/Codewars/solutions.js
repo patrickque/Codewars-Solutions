357e2b153641c0d79fb3b3fd49d9da521cd10140
Patrick Pisuc <pisucpatrick@mail.com>
2022-08-01 19:28:44 +0300

    Beginner - lost without a map

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -438,3 +438,14 @@ function stringToArray(string){
 function digitize(num) {
   return Array.from(String(num), Number).reverse()
 }
+
+////Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+//solution
+function maps(x){
+  return x.map((n,i) => n*2)
+}
