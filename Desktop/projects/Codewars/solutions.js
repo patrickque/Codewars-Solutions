a5d1d032d5d4b6cc6386916fbbe6a4debaf49fc4
Patrick Pisuc <pisucpatrick@mail.com>
2022-08-02 20:49:24 +0300

    Sentence smash

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -503,3 +503,15 @@ function fakeBin(x){
 String.prototype.toJadenCase = function () {
   return this.split(' ').map((n, i) => n[0].toUpperCase() + n.slice(1)).join(' ')
 };
+
+
+////Sentence Smash
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+// Example
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+//solution
+function smash (words) {
+  return words.join(' ')
+};
