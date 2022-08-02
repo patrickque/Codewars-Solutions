9fc53bb238221134fefdb84800463b5c4c051df4
Patrick Pisuc <pisucpatrick@mail.com>
2022-08-02 21:14:31 +0300

    Beginner series #4 Cockroach

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -539,3 +539,16 @@ function minMax(arr){
    result.push(highest)
   return result
 }
+
+
+////The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+// For example:
+
+// 1.08 --> 30
+// Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
+
+//solution
+function cockroachSpeed(s) {
+  let cmSpeed = s * 100000/3600
+  return Math.floor(cmSpeed)
+}
