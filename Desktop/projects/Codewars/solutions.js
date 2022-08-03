6b7e0abe27996d02bf81cde1c75dde025cd3c4c3
Patrick Pisuc <pisucpatrick@mail.com>
2022-08-03 20:45:07 +0300

    Sum Mixed Array

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -595,3 +595,10 @@ function getAge(inputString){
   return Number(inputString[0])
 }
 
+////Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+// Return your answer as a number.
+
+//solution
+function sumMix(x){
+  return x.map(num => Number(num)).reduce((acc, c) => acc + c)
+}
