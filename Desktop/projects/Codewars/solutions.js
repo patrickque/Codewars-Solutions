2031b72cce72abfeabe4436c412ec367e2f26b0b
Patrick Pisuc <pisucpatrick@mail.com>
2022-08-04 09:14:11 +0300

    Filter unused digits

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -667,6 +667,20 @@ function numberToString(num) {
 
 //solution
 function gooseFilter (birds) {
-  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  let geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
   return birds.filter(word => !geese.includes(word))
 };
+
+
+////Given a varying number of integer arguments, return the digits that are not present in any of them.
+// Example:
+// [12, 34, 56, 78]  =>  "09"
+// [2015, 8, 26]     =>  "3479"
+// Note: the digits in the resulting string should be sorted.
+
+//solution
+function unusedDigits(...num) {
+  const digits = [0,1,2,3,4,5,6,7,8,9]
+  const numArrayToString = num.join('')  
+  return digits.filter(digit => !numArrayToString.includes(digit)).join('')
+}
