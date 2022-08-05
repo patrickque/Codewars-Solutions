f1622969c3fecf208ffbb33cae003b2f316395d9
Patrick Pisuc <pisucpatrick@mail.com>
2022-08-05 17:41:54 +0300

    Multiply array values and filter non-numeric

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -702,3 +702,14 @@ var FilterString = function(value) {
   //Complete this function :)
   return +value.split('').filter(n => !isNaN(n)).join('')
 }
+
+
+////Multiply array values and filter non-numeric
+// Your task is to write a function, which takes two arguments and returns a sequence. First argument is a sequence of values, second is multiplier. The function should filter all non-numeric values and multiply the rest by given multiplier.
+
+//solution
+function multiplyAndFilter(array, multiplier){
+  // your code
+  return array.filter(el => typeof(el) === 'number').map(el => el * multiplier );
+}
+
