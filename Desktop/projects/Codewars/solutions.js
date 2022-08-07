927ad0a3561d83512c559b45a50bf78ad95871af
Patrick Pisuc <pisucpatrick@mail.com>
2022-08-07 19:14:30 +0300

    filter the array into unique values in 38 chars

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -713,3 +713,13 @@ function multiplyAndFilter(array, multiplier){
   return array.filter(el => typeof(el) === 'number').map(el => el * multiplier );
 }
 
+
+//// Filter the array into unique values. You have 38 characters to do it in!
+// Input => [1, 1, 2, 3]
+// Output => [1, 2, 3]
+// Note:
+// It can be achieved in 29 characters if you want to make some truly horrendous code.
+// If you use let or const or other ES6 features, your code will get longer due to the transformation by babel.
+
+//solution
+const makeUnique=a=>[...new Set(a)]
