d8a968457d182aad83bc5b7124e9d66d5fff5278
Patrick Pisuc <pisucpatrick@mail.com>
2022-08-08 21:30:03 +0300

    Use map() to double the values in an array

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -805,3 +805,14 @@ Array.prototype.inRange = function(min,max){
   return this.filter(el => Number.isInteger(el) && el >= min && el <= max)
 }
 
+
+////Make the double() function return a new array with each value twice as large as the corresponding value in the passed in array. Your solution must use the map() function of the built-in javascript Array object. If you're not familiar with map(), reading over the documentation may help.
+// For example:
+// var someNumbers = [1,2,10,57];
+// double(someNumbers); // should return [2,4,20,114]
+
+
+//solution
+function double(array) {
+  return array.map(el => el*2)
+}
