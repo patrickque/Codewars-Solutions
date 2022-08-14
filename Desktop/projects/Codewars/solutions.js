2292caed6b55d0093174c3b603f023b26f47017e
Patrick Pisuc <pisucpatrick@mail.com>
2022-08-14 09:19:57 +0300

    Is this my tail?

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -1012,3 +1012,26 @@ function addBinary(a,b) {
   let sum = a + b;
   return sum.toString(2);
 }
+
+////Some new animals have arrived at the zoo. The zoo keeper is concerned that perhaps the animals do not have the right tails. To help her, you must correct the broken function to make sure that the second argument (tail), is the same as the last letter of the first argument (body) - otherwise the tail wouldn't fit!
+// If the tail is right return true, else return false.
+// The arguments will always be non empty strings, and normal letters.
+//    function correctTail(bod, tail ;
+//       sub = body.substr(bodylength-(tail.length)
+//        if sub = tail) ;
+//          return true
+//         }
+//        else 
+//          return false
+
+//solution
+function correctTail(body, tail){
+  let sub = body.substr(body.length - tail.length);
+    if (sub === tail){
+      return true;
+    } else {
+      return false;
+    }
+}
+  
+  
