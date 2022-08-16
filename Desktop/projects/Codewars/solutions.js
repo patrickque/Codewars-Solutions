28fb9b4c677deb885d263e0bd059eca1ea6d2b6e
Patrick Pisuc <pisucpatrick@mail.com>
2022-08-16 21:31:51 +0300

    Square Every Digit

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -1034,4 +1034,12 @@ function correctTail(body, tail){
     }
 }
   
-  
+
+////Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+// Note: The function accepts an integer and returns an integer
+
+//solution
+function squareDigits(num){
+  return Number(num.toString().split('').map((digit) => digit * digit).join(''));
+}
