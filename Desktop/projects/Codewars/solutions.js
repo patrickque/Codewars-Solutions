5f0b3825cbb69db4d5980080e02f813c1518842b
Patrick Pisuc <pisucpatrick@mail.com>
2022-08-18 19:47:01 +0300

    Calculate BMI

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -1096,3 +1096,24 @@ const multiply = (x, y) => x * y;
 function isIsogram(str){
   return str.length === new Set(str.toLowerCase()).size;
 }
+
+
+////Write function bmi that calculates body mass index (bmi = weight / height2).
+// if bmi <= 18.5 return "Underweight"
+// if bmi <= 25.0 return "Normal"
+// if bmi <= 30.0 return "Overweight"
+// if bmi > 30 return "Obese"
+
+//solution
+function bmi(weight, height) {
+  let bmIndex = weight / (height * height)
+    if (bmIndex <= 18.5) {
+      return 'Underweight'
+    } else if(bmIndex <= 25.0) {
+      return 'Normal'
+    } else if(bmIndex <= 30.0) {
+      return 'Overweight'
+    } else if(bmIndex > 30) {
+      return 'Obese'
+    }
+}
