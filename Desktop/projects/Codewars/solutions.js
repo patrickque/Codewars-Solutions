f9470fe0b10195fd6262d2b86781737728fc4fbf
Patrick Pisuc <pisucpatrick@mail.com>
2022-08-18 19:30:04 +0300

    Isograms

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -1084,3 +1084,15 @@ function removeRotten(bagOfFruits){
 
 //solution
 const multiply = (x, y) => x * y;
+
+
+////An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+// Example: (Input --> Output)
+// "Dermatoglyphics" --> true
+// "aba" --> false
+// "moOse" --> false (ignore letter case)
+
+//solution
+function isIsogram(str){
+  return str.length === new Set(str.toLowerCase()).size;
+}
