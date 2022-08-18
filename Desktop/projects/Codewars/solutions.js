8803478c3a3a9624175797112ec1e87055ad5260
Patrick Pisuc <pisucpatrick@mail.com>
2022-08-18 18:56:57 +0300

    Function 3 - multiplying two numbers

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -1075,3 +1075,12 @@ function spacify(str) {
 function removeRotten(bagOfFruits){
   return bagOfFruits ? bagOfFruits.map((el) => el.replace('rotten', '').toLowerCase()) : [];
 }
+
+
+////Implement a function called multiply, which takes two numbers and returns their product:
+// multiply(2, 3) = 6
+// multiply(0, 188) = 0
+// multiply(85, 144) = 12240
+
+//solution
+const multiply = (x, y) => x * y;
