791ddea98b2e764b73213b3c23c329813c31cf7a
Patrick Pisuc <pisucpatrick@mail.com>
2022-08-19 17:04:16 +0300

    Transportation on vacation

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -1117,3 +1117,21 @@ function bmi(weight, height) {
       return 'Obese'
     }
 }
+
+
+////After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
+// You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+// Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+// Write a code that gives out the total amount for different days(d).
+
+//solution
+function rentalCarCost(days) {
+  let pricePerDay = 40;
+    if (days < 3) {
+      return (pricePerDay * days)
+  } else if (days >= 3 && days < 7) {
+      return (pricePerDay * days) - 20
+  } else if (days >= 7) {
+      return (pricePerDay * days) - 50
+  }
+}
