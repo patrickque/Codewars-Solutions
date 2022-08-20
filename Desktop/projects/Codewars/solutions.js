ae9f0637d31f9a360d4cb76b0cd0af9b28bbe8ba
Patrick Pisuc <pisucpatrick@mail.com>
2022-08-20 17:42:58 +0300

    Get the mean of an array

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -1157,3 +1157,15 @@ function fixTheMeerkat(arr) {
  function fireFight(s){
   return s.replace(/Fire/g, '~~');
 }
+
+
+////It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+// Return the average of the given array rounded down to its nearest integer.
+// The array will never be empty.
+
+//solution
+function getAverage(marks){
+  return Math.floor(marks.reduce((a, b) => a + b) / marks.length);
+}
+
+
