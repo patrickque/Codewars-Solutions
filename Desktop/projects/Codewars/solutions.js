ff6fd4b9551871dcb4093fc048f3a4a3847f7a63
Patrick Pisuc <pisucpatrick@mail.com>
2022-08-20 14:06:24 +0300

    Holiday III - Fire on the boat

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -1146,3 +1146,14 @@ function rentalCarCost(days) {
 function fixTheMeerkat(arr) {
   return arr.reverse();
  }
+
+
+ ////Enjoying your holiday, you head out on a scuba diving trip!
+// Disaster!! The boat has caught fire!!
+// You will be provided a string that lists many boat related items. If any of these items are "Fire" you must spring into action. Change any instance of "Fire" into "~~". Then return the string.
+// Go to work!
+
+ //solution
+ function fireFight(s){
+  return s.replace(/Fire/g, '~~');
+}
