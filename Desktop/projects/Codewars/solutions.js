4eb9d01675dafcab7dfff13609b2620e58ee7b4e
Patrick Pisuc <pisucpatrick@mail.com>
2022-08-24 15:24:57 +0300

    Small enough?

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -1225,3 +1225,12 @@ function NameMe(first, last) {
 function divisibleBy(numbers, divisor){
   return numbers.filter(n => n % divisor === 0);
 }
+
+
+////You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+// You can assume all values in the array are numbers.
+
+//solution
+function smallEnough(a, limit){
+  return a.every(el => el <= limit);
+}
