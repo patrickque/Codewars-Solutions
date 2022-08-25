c6a48b12738b6181f7b242dc083af37e5cbb1f22
Patrick Pisuc <pisucpatrick@mail.com>
2022-08-25 17:17:00 +0300

    Testing 1-2-3

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -1296,3 +1296,12 @@ function redistributeWealth(wealth) {
 }
 
 
+////Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+// Examples: (Input --> Output)
+
+//solution
+const number = function(array){
+  return array.map((el, index) => `${index + 1}: ${el}`)
+}
