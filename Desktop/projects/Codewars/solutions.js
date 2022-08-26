3a4d9f8ac3c5cd5638eb5cc44e884893d1bd83f6
Patrick Pisuc <pisucpatrick@mail.com>
2022-08-26 15:18:55 +0300

    Dropcaps

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -1321,3 +1321,21 @@ function arr(N) {
   }
   return result;
 }
+
+
+////DropCaps means that the first letter of the starting word of the paragraph should be in caps and the remaining lowercase, just like you see in the newspaper.
+
+// But for a change, let"s do that for each and every word of the given String. Your task is to capitalize every word that has length greater than 2, leaving smaller words as they are.
+
+// *should work also on Leading and Trailing Spaces and caps.
+
+// "apple"            => "Apple"
+// "apple of banana"  => "Apple of Banana"
+// "one   space"      => "One   Space 
+// "   space WALK   " => "   Space Walk   " 
+// Note: you will be provided atleast one word and should take string as input and return string as output.
+
+//solution
+function dropCap(str) {
+  return str.split(' ').map(el => el.length > 2 ? el[0].toUpperCase() + el.slice(1).toLowerCase() : el).join(' ');
+}
