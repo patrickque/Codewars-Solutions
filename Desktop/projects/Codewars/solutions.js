2b17f577e9efa64046ceda1e2a8159f52e0e1f6c
Patrick Pisuc <pisucpatrick@mail.com>
2022-08-26 15:04:40 +0300

    Fillin an array (part 1)

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -1305,3 +1305,19 @@ function redistributeWealth(wealth) {
 const number = function(array){
   return array.map((el, index) => `${index + 1}: ${el}`)
 }
+
+
+////We want an array, but not just any old array, an array with contents!
+// Write a function that produces an array with the numbers 0 to N-1 in it.
+// For example, the following code will result in an array containing the numbers 0 to 4:
+// arr(5) // => [0,1,2,3,4]
+// Note: The parameter is optional. So you have to give it a default value.
+
+//solution
+function arr(N) {
+  let result = [];
+  for (let i = 0; i < N; i++) {
+    result.push(i);
+  }
+  return result;
+}
