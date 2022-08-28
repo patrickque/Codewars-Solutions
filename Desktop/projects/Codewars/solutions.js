707d2f1e1394d1d6d83d0a59a851686e5ec5b11c
Patrick Pisuc <pisucpatrick@mail.com>
2022-08-28 10:10:34 +0300

    Length and two values

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -1357,3 +1357,20 @@ const capitals = function (word) {
   }
   return result;
 };
+
+
+////Given an integer n and two other values, build an array of size n filled with these two values alternating.
+
+// Examples
+// 5, true, false     -->  [true, false, true, false, true]
+// 10, "blue", "red"  -->  ["blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red"]
+// 0, "one", "two"    -->  []
+
+//solution
+function alternate(n, firstValue, secondValue){
+  let result = [];
+  for (let i = 0; i < n; i++) {
+    result.push(i % 2 === 0 ? firstValue : secondValue)
+  }
+  return result;
+}
