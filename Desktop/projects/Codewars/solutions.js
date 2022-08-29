63312127ba572119e2e24864cb31b5029bc37fbd
Patrick Pisuc <pisucpatrick@mail.com>
2022-08-29 12:40:03 +0300

    Last Survivor

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -1444,3 +1444,32 @@ function strangeMath(n, k){
   }
   return numbers.sort().indexOf(k) + 1;
 }
+
+
+////You are given a string of letters and an array of numbers.
+// The numbers indicate positions of letters that must be removed, in order, starting from the beginning of the array.
+// After each removal the size of the string decreases (there is no empty space).
+// Return the only letter left.
+
+// Example:
+
+// let str = "zbk", arr = [0, 1]
+//     str = "bk", arr = [1]
+//     str = "b", arr = []
+//     return 'b'
+// Notes
+// The given string will never be empty.
+// The length of the array is always one less than the length of the string.
+// All numbers are valid.
+// There can be duplicate letters and numbers.
+// If you like this kata, check out the next one: Last Survivors Ep.2
+
+//solution
+function lastSurvivor(letters, coords) {
+  letters = letters.split('')
+  console.log(letters)
+  for (let i = 0; i < coords.length; i++) {
+    letters.splice(coords[i], 1)
+  }
+  return letters.join('');
+}
