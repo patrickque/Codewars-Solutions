b9760d2f273ac0f83fa3c61c18caee500b446875
Patrick Pisuc <pisucpatrick@mail.com>
2022-08-29 12:08:00 +0300

    Strange mathematics

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -1359,6 +1359,7 @@ const capitals = function (word) {
 };
 
 
+
 ////Given an integer n and two other values, build an array of size n filled with these two values alternating.
 
 // Examples
@@ -1413,4 +1414,33 @@ function points(games) {
 //solution
 function arrayPlusArray(arr1, arr2) {
   return arr1.reduce((a, b) => a + b) + arr2.reduce((a, b) => a + b);
+}
+
+
+////In a certain kingdom, strange mathematics is taught at school. Its main difference from ordinary mathematics is that the numbers in it are not ordered in ascending order, but lexicographically, as in a dictionary (first by the first digit, then, if the first digit is equal, by the second, and so on). In addition, we do not consider an infinite set of natural numbers, but only the first n numbers.
+
+// So, for example, if n = 11, then the numbers in strange mathematics are ordered as follows:
+
+// 1, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9.
+
+// Help your students to learn this science: write a function that receives two integer numbers: n (total amount of numbers in strange mathematics) and k (number from sequence) and returns the location of a given number k in the order defined in strange mathematics.
+
+// For example, if n = 11 and k = 2, the function should return 4 as the answer.
+
+// Input: 1 <= n <= 100 000 , 1 <= k <= n.
+
+// Output: position of the number k in sequence of the first n natural numbers in lexicographic order. Numbering starts with 1.
+
+// Examples:
+// strangeMath(11, 2) === 4
+// strangeMath(15, 5) === 11
+// strangeMath(15, 15) === 7
+
+//solution
+function strangeMath(n, k){
+  let numbers = []
+  for (let i = 1; i <= n; i++){
+    numbers.push(i);
+  }
+  return numbers.sort().indexOf(k) + 1;
 }
