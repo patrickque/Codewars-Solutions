808530779d13ad9a73196c826067518deae5f62e
Patrick Pisuc <pisucpatrick@mail.com>
2022-09-02 19:04:55 +0300

    Basic training: add item to an array

--- Desktop/projects/Codewars/solutions.js
+++ Desktop/projects/Codewars/solutions.js
@@ -1536,3 +1536,14 @@ function duplicates(array){
 function clean(arr) {
   return arr.filter(el => true);
 }
+
+
+////Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+// The websites array has already been defined for you using the following code:
+
+let websites = [];
+
+//solution
+// add the value "codewars" to the already defined websites array
+websites.push('codewars')
